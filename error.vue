4a52d76463cdf2adc605fc1b0ab22aaf983d06a1
mezitomi <mezitomi@gmail.com>
2025-06-09 17:58:17 +0200

    feat: add 403, 404, generic error page

--- error.vue
+++ error.vue
@@ -1,23 +1,42 @@
 <script setup lang="ts">
 import type { NuxtError } from "#app";
 
-defineProps({
+const props = defineProps({
   error: Object as () => NuxtError,
 });
 
 const localePath = useLocalePath();
+if (props.error?.statusCode === 404) {
+  clearError({ redirect: localePath("/404") });
+}
 
+if (props.error?.statusCode === 403) {
+  clearError({ redirect: localePath("/403") });
+}
 const handleError = () => clearError({ redirect: localePath("index") });
 </script>
 
 <template>
-  <div>
+  <div class="container">
     <h2>
-      Oh snap! Something went wrong.
-      {{ error?.statusCode }}
+      {{ $t("pages.error.title") }}
     </h2>
+    <h3>{{ $t("pages.error.subtitle") }}</h3>
     <VaButton @click="handleError">
-      {{ $t("navbar.index") }}
+      {{ $t("pages.error.backToHome") }}
     </VaButton>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    inline-size: 30%;
+  }
+}
+</style>
